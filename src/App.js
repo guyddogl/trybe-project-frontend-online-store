b00fc267bfb34e0127af67dcb66fac6093e8ba10
Guyddo <guyddogl@gmail.com>
2022-08-05 20:39:31 -0300

    Add main container-fluid

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
-// import { getProductsFromCategoryAndQuery } from './services/api';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route exact path="/cart" component={ ShoppingCart } />
-      </Switch>
+      <main className="container-fluid">
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/cart" component={ ShoppingCart } />
+        </Switch>
+        <Footer />
+      </main>
     </BrowserRouter>
   );
 }
