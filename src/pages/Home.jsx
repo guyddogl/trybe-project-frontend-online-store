7f53bcb2548282a1768ebeb822257b103577dfa1
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-08-05 17:09:45 -0300

    Merge req 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Categories from '../components/Categories';
 
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
         <Link to="/cart">
           <button type="submit" data-testid="shopping-cart-button">Carrinho</button>
         </Link>
+        <Categories />
         <p
           data-testid="home-initial-message"
         >
