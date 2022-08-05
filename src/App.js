378290e5476253646077153674f18aeafd1094ad
Guyddo <guyddogl@gmail.com>
2022-08-05 20:57:37 -0300

    Create Header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
+import Header from './components/Header';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <main className="container-fluid">
+        <Header />
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/cart" component={ ShoppingCart } />
