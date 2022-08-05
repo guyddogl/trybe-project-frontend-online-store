14b4181d1b5b3c18a1dfa4bc028f500ba6262390
Guyddo <guyddogl@gmail.com>
2022-08-05 15:42:44 -0300

    Requeriment 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,26 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const result = await fetch(URL);
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    return 'You must provide an url';
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  let url = '';
+  if (categoryId) {
+    url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  } else {
+    url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  }
+  try {
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    return 'You must provide an url';
+  }
 }
