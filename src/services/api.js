c0938e1d8bfd3447f65296f49ce99e5b0708e740
Lucas Israel <lisraelalves92@gmail.com>
2022-08-05 21:45:56 -0300

    refatornado funcoes da api

--- src/services/api.js
+++ src/services/api.js
@@ -5,27 +5,19 @@ export async function getCategories() {
     const data = await result.json();
     return data;
   } catch (error) {
-    return 'You must provide an url';
+    return error;
   }
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
-  // let url = '';
-  // if (categoryId) {
-  //   url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
-  // } else {
-  //   url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  // }
-  const queryCategory = categoryId
-    ? `category=${categoryId}`
-    : '';
-  const queryInput = query ? `q=${encodeURI(query)}` : '';
-  const url = `https://api.mercadolibre.com/sites/MLB/search?${queryCategory}${queryCategory && queryInput ? '&' : ''}${queryInput}`;
+export async function getProductsFromCategoryAndQuery(categoryId = '', search = '') {
+  const queryCategory = categoryId && `category=${categoryId}`;
+  const queryInput = search && `q=${encodeURI(search)}`;
+  const url = `https://api.mercadolibre.com/sites/MLB/search?${queryCategory}${queryCategory && queryInput && '&'}${queryInput}`;
   try {
     const result = await fetch(url);
     const data = await result.json();
     return data;
   } catch (error) {
-    return 'You must provide an url';
+    return error;
   }
 }
